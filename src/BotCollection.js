279a95b98994a606faf765cfbf80b786f249605e
code-iddih <idinyamwaya@gmail.com>
2024-08-11 17:05:17 +0300

    Adds Function to display Bots

---
+++ src/BotCollection.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import BotCard from './BotCard';
+
+// Function to displays the collection of bots
+
+const BotCollection = ({ bots, onAddToArmy, onRemoveFromCollection }) => {
+  return (
+    <div className="bot-collection-grid">
+      {bots.map(bot => (
+        <BotCard 
+          key={bot.id} 
+          bot={bot} 
+          onAddToArmy={onAddToArmy} 
+          onRemoveFromCollection={onRemoveFromCollection} 
+        />
+      ))}
+    </div>
+  );
+};
+
+export default BotCollection;
